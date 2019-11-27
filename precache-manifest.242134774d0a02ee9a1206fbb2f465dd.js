fd90c36858f35b5a9caa966c4ae221ac9b2eab64
Darshan Datta <darshandatta.10@gmail.com>
2019-11-28 01:04:51 +0530

    Updates

---
+++ precache-manifest.242134774d0a02ee9a1206fbb2f465dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "603187431f9f27f55691210553f26521",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "cce6a738af5de410ad00",
+    "url": "/react-todo/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "48d5f7202d48ff36139b",
+    "url": "/react-todo/static/js/2.0e5faaf8.chunk.js"
+  },
+  {
+    "revision": "cce6a738af5de410ad00",
+    "url": "/react-todo/static/js/main.42b1b803.chunk.js"
+  },
+  {
+    "revision": "460e1f2afce5d90d8bdc",
+    "url": "/react-todo/static/js/runtime-main.8c3598db.js"
+  }
+]);
